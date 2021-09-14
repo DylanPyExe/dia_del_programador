de045669ce8d98b6c2fc59422246b742411e8103
Dylan.py <89953625+DylanPyExe@users.noreply.github.com>
2021-09-13 22:08:22 -0300

    Create programador.py

---
+++ programador.py
@@ -0,0 +1,72 @@
+import time
+import os
+def hackcoders():
+	print("\033[33m")
+	time.sleep(0.5)
+	print("\t\t\tHonor hackcoders:3")
+	print(""" ...,,,**///   .  .*((((((////**,,,.. 
+ ..,,**///   .     ......./%###((//***,,
+.,,*///        .    .........&%%%##(//**
+,*//((      ......,,,..,...,../&&&%%#((/
+*/((#   .                    .*@@@&&%%#(
+/(##                             ,@@&%%#
+(.                        .   .   ..@&%%
+(#%  /,,    . ./****//(,....,,*%/@@@@&&%
+(#%%*(/*,... ,*,  (# /##%%%#&&&&&@@@@&&%
+(#%%&/*.,,//,.,.,,(,##/(cholohatwhite@@&
+(#%  **,./**,.,*,,,,*//###%&&%##,,..&&%%
+      *,**/*,**.,*   ,/#%%@&%%%...    .#
+       ,.*/***...*,,,  .#%@&&%   .    #(
+         ..,.   */*//...*.,(*       ((//
+                  ./. *(##/        (//**
+                  ./,//(,*.         *,,.""")
+time.sleep(0.6)     
+hackcoders()  
+print("\033[34m")
+print("\n\n\nFeliz dia del programador!")     
+print("\033[36m")      
+
+                  
+                                    
+                                                               
+                                                                                 
+print("13 de septiembre, lunes")    
+time.sleep(0.4)   
+print("se festeja el dia del programador")       
+time.sleep(4) 
+os.system("clear")
+print("\033[33m")
+print("\n\n\nTambien para hackcoders!")
+print(""" ...,,,**///   .  .*((((((////**,,,.. 
+ ..,,**///   .     ......./%###((//***,,
+.,,*///        .    .........&%%%##(//**
+,*//((      ......,,,..,...,../&&&%%#((/
+*/((#   .                    .*@@@&&%%#(
+/(##                             ,@@&%%#
+(.                        .   .   ..@&%%
+(#%  /,,    . ./****//(,....,,*%/@@@@&&%
+(#%%*(/*,... ,*,  (# /##%%%#&&&&&@@@@&&%
+(#%%&/*.,,//,.,.,,(,##/(cholohatwhite@@&
+(#%  **,./**,.,*,,,,*//###%&&%##,,..&&%%
+      *,**/*,**.,*   ,/#%%@&%%%...    .#
+       ,.*/***...*,,,  .#%@&&%   .    #(
+         ..,.   */*//...*.,(*       ((//
+                  ./. *(##/        (//**
+                  ./,//(,*.         *,,.""")
+print("para todos los programadores de todo tipos de lenjuages python, c++, c, visual net, go, haskell y todo tipo les deseo feliz dia🙌")
+time.sleep(6) 
+os.system("clear") 
+def nya():
+	time.sleep(1)  
+	print("feliz dia muchachos")  
+	time.sleep(1)  
+	print("espero que la pasen bien este dia especial para los programadores")   
+time.sleep(1)
+print("no dejen que nada les arruine el dia muchachos")
+time.sleep(1)
+print("esperamos este dia hace bastante")
+time.sleep(1)
+print("y ya llego, a mostrar proyectos:)")
+time.sleep(1)
+print("bendiciones a todos los programadores de todos los paises!")
+
